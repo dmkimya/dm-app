e22226f3738c946e94b5f261239d612164e8ffcd
Emre <pi.emree@gmail.com>
2022-03-23 19:11:42 +0300

    env fix

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,13 @@
 
 export default {
-    apiKey: process.env.firebaseApiKey,
-    authDomain:process.env.firebaseAuthDomain ,
-    databaseURL:process.env.firebaseDbUrl ,
-    projectId:process.env.firebaseProjectId,
-    storageBucket:process.env.firebaseStorageBucket,
-    messagingSenderId:process.env.firebaseMessagingSenderId,
-    appId:process.env.firebaseAppId ,
-    measurementId:process.env.firebaseMeasurementId
+  apiKey: process.env.firebaseApiKey,
+  authDomain:process.env.firebaseAuthDomain ,
+  databaseURL:process.env.firebaseDbUrl ,
+  projectId:process.env.firebaseProjectId,
+  storageBucket:process.env.firebaseStorageBucket,
+  messagingSenderId:process.env.firebaseMessagingSenderId,
+  appId:process.env.firebaseAppId ,
+  measurementId:process.env.firebaseMeasurementId
   };
 
 
